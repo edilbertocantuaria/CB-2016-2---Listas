71757fa01aeef5acf22e17e0c94f3857261bb2b1
Edilberto Cantuaria <69125218+edilbertocantuaria@users.noreply.github.com>
2023-02-03 09:08:37 -0300

    Delete Respostas directory

--- Respostas/L1E3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main (){
-       double salario1;
-scanf ("%lf", & salario1);
-       float aumento = (salario1*0.25);
-        double sf = aumento + salario1;
-printf ("%.2lf \n", sf);
-return 0;
-}
